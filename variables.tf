8e4e6792da367b9efebdf12ec3f361c03bc03948
softstuck <52829595+softstuck@users.noreply.github.com>
2026-05-28 12:26:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,8 +19,8 @@ variable "aws_region" {
 
 variable "ami" {
    type        = string
-   description = "Ubuntu AMI ID"
-   default     = "ami-0f5ee92e2d63afc18"
+   description = "AmazonLinux AMI ID"
+   default     = "ami-078f95be0757084a3"
 }
 
 variable "instance_type" {
